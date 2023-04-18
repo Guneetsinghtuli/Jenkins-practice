814d40c7573d12eac86869c8061f54b62a690559
GUNEET SINGH <guneetsinghtuli@gmail.com>
2023-04-19 02:05:34 +0530

    maven moved

---
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -0,0 +1,16 @@
+package com.mycompany.app;
+
+
+import org.junit.Test;
+
+import static junit.framework.Assert.assertTrue;
+
+class AppTest {
+
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+
+}
